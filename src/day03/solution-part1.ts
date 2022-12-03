19c9d825acd3feb012e3f64fb859802969f53272
Rostislav Klein <rostaklein@gmail.com>
2022-12-03 10:34:34 +0100

    day 3 part 1 done

---
+++ src/day03/solution-part1.ts
@@ -0,0 +1,53 @@
+import { loadInputData } from "../loadInputData.ts";
+
+const input = await loadInputData("03");
+
+const getLetterValue = (letter: string) => {
+  const charCode = letter.charCodeAt(0);
+  const isUpperCase = charCode <= 90;
+
+  return isUpperCase ? charCode - 38 : charCode - 96;
+};
+
+const getRucksacks = () => {
+  return input
+    .map((line) => ({
+      first: line
+        .split("")
+        .splice(0, line.length / 2)
+        .join(""),
+      second: line
+        .split("")
+        .slice(line.length / 2)
+        .join(""),
+    }))
+    .reduce<
+      Array<{
+        first: string;
+        second: string;
+        sameLetter: string;
+        value: number;
+      }>
+    >((acc, curr) => {
+      const sameLetter = curr.first
+        .split("")
+        .find((letter) => curr.second.split("").includes(letter));
+
+      if (!sameLetter) {
+        throw new Error("No letter is the same in first and second rucksack");
+      }
+
+      acc.push({
+        ...curr,
+        sameLetter,
+        value: getLetterValue(sameLetter),
+      });
+
+      return acc;
+    }, [])
+    .reduce((acc, curr) => {
+      return (acc += curr.value);
+    }, 0);
+};
+
+console.log(getRucksacks());
