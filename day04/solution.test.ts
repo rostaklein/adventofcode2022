a1a39b2474911e90e9d85744c21d9730f07595fd
Rostislav Klein <rostaklein@gmail.com>
2022-12-04 17:45:52 +0100

    day 4 part 2 done

--- day04/solution.test.ts
+++ day04/solution.test.ts
@@ -1,9 +1,14 @@
 import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
 import { loadInputData } from "../loadInputData.ts";
-import { getSolution } from "./solution-part1.ts";
+import { getSolution as part1 } from "./solution-part1.ts";
+import { getSolution as part2 } from "./solution-part2.ts";
 
 const input = await loadInputData("04", "demo");
 
 Deno.test("solution 1 works", () => {
-  assertEquals(getSolution(input), 2);
+  assertEquals(part1(input), 2);
+});
+
+Deno.test("solution 2 works", () => {
+  assertEquals(part2(input), 4);
 });
